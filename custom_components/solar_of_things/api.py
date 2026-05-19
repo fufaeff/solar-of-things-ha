02d7f208ace20c7d5bbb39b4cc7ae7569664f259
Fufaev Andrew <fufaev@specialist-it.com.ua>
2026-05-19 21:17:53 +0300

    Test

--- custom_components/solar_of_things/api.py
+++ custom_components/solar_of_things/api.py
@@ -662,7 +662,7 @@ class SolarOfThingsAPI:
         if "acOutputActivePower" in latest_values:
             try:
                 latest_values["acOutputActivePower"] = (
-                    float(latest_values["acOutputActivePower"]) * 1000.0
+                    float(float(latest_values["acOutputActivePower"])) * 1000.0
                 )
             except Exception:
                 pass
